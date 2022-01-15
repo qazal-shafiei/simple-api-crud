6524b533aac2d49c96aa55ffbcbf9f0c57d1b402
qazal-shafiei <ghazalshafiii@gmail.com>
2022-01-15 17:15:31 +0330

    (feat): added status code

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -74,6 +74,6 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         $product->delete();
-        return response(['message' => 'deleted successfully!']);
+        return response(['message' => 'deleted successfully!'], 200);
     }
 }
