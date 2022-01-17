bf36f208eca0429209469cac7e56b2430d55d48d
qazal-shafiei <ghazalshafiii@gmail.com>
2022-01-17 17:03:31 +0330

    fixed UserTest

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,48 +5,28 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\User;
-use Faker\Generator as Faker;
-use JWTAuth;
-use Config;
 
 class UserTest extends TestCase
 {
     use WithFaker;
-
-    public function test_user_delete()
+    public function test_registration()
     {
-        $user = User::first();
-        $user ? $user->delete() : false;
-        $this->assertTrue(true);
-    }
-
-    public function test_user_registration()
-    {
-
         $response = $this->post('/api/register', [
-            'name' => $this->faker->name,
-            'email' => $this->faker->email,
-            'password' => 123456,
-            'password_confirmation' => 123456,
+           'name' => $this->faker->name,
+           'email' => $this->faker->email,
+           'password' => 123456,
+           'password_confirmation' => 123456
         ]);
 
         $response->assertStatus(201);
     }
 
-    public function test_user_login()
+    public function test_login()
     {
-
-    $response = $this->json('POST', '/api/login' . '/', [
-        'email' => 'qazal@gmail.com',
-        'password' => '123456'
-    ]);
-    $accessToken = JWTAuth::attempt($loginData);
-
-    $response
-        ->assertStatus(200)
-        ->assertJsonStructure([
-            'accessToken', 'token_type', 'expires_in'
+        $response = $this->post('api/login', [
+            'email' => 'emaill@email.com',
+            'password' => 123456
         ]);
+        $response->assertStatus(200);
     }
 }
