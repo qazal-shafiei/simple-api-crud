c7d7e7c7ac3c39cc715cf0ec48f7d802d7e6d9f7
qazal-shafiei <ghazalshafiii@gmail.com>
2022-01-18 15:13:29 +0330

    Completed ProductTest

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,18 +2,50 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Product;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use JWTAuth;
 
 class ProductTest extends TestCase
 {
-
+    use WithFaker;
+    protected function authorization()
+    {
+        $user = User::factory()->create();
+        $token = JWTAuth::fromUser($user);
+        return [
+            'user' => $user,
+            'token' => $token
+        ];
+    }
     public function test_show_products()
     {
-        $user = User::where('email', 'elvie40@gmail.com ')->first();
-        $response = $this->actingAs($user)->get('/api/products');
+        $response = $this->call('GET','/api/products',
+            [], [],[],['HTTP_Authorization' => 'Bearer ' . $this->authorization()['token']],[]);
+        $response->assertStatus(200);
+    }
+
+    public function test_store_product()
+    {
+        $response = $this->call('POST','/api/products',
+            ["name" => $this->faker->firstName],[],[],['HTTP_Authorization' => 'Bearer ' . $this->authorization()['token']],[]);
+        $response->assertStatus(201);
+    }
+
+    public function test_update_product()
+    {
+        $response = $this->call('PUT','/api/products/27',
+            ["name" => $this->faker->firstName],[],[],['HTTP_Authorization' => 'Bearer ' . $this->authorization()['token']],[]);
+        $response->assertStatus(200);
+    }
+
+    public function test_delete_product()
+    {
+        $product = Product::all()->last();
+        $response = $this->call('DELETE', '/api/products/'. $product->id,
+            [], [], [], ['HTTP_Authorization' => 'Bearer' . $this->authorization()['token']], []);
         $response->assertStatus(200);
     }
 }
